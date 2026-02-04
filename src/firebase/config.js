b4ab458cb18353b897b9beaf03eb9bf1c119e1c1
Andrea Agustina Castillo <andreacastillo2022@gmail.com>
2026-02-04 16:32:49 -0300

    configuración inicial de Firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDK6zNqgashWDhdrlRBqWH5gMnzFrb_gKE",
+  authDomain: "react-productos-27c38.firebaseapp.com",
+  projectId: "react-productos-27c38",
+  storageBucket: "react-productos-27c38.firebasestorage.app",
+  messagingSenderId: "817288977641",
+  appId: "1:817288977641:web:fc21b59d7f4a9e5f266478"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
