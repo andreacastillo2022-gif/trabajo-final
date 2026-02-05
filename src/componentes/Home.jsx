730c6f7395e2da44a4a869963a6c7d249188387f
Andrea Agustina Castillo <andreacastillo2022@gmail.com>
2026-02-04 21:59:44 -0300

    Estructura de Home + ingreso de imagenes de productos

--- src/componentes/Home.jsx
+++ src/componentes/Home.jsx
@@ -1,9 +1,35 @@
+
+import productos from "./productos";
+
 function Home() {
   return (
-    <div>
-      Home
+    <div className="home-container">
+      
+      <nav className="nav">
+        <h2 className="logo">Mi Tienda</h2>
+        <button className="logout">Salir</button>
+      </nav>
+
+      <main className="main">
+            <h1>Productos</h1>
+
+            <div className="productos">
+            {productos.map((prod) => (
+                <div key={prod.id} className="producto">
+                    <h3>{prod.nombre}</h3>
+                    <p>${prod.preciosinensamblar}</p>
+                    <p>${prod.precioensamblado}</p>
+                </div>
+                ))}
+            
+            </div>
+      </main>
+
     </div>
   );
 }
 
 export default Home;
+
+
+  
