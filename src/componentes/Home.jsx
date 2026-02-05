a0476838bd3eba5bd148316061994d5c82e00560
Andrea Agustina Castillo <andreacastillo2022@gmail.com>
2026-02-04 23:02:05 -0300

    Cambios al CSS y arreglo de errores en productos.js

--- src/componentes/Home.jsx
+++ src/componentes/Home.jsx
@@ -19,7 +19,9 @@ function Home() {
                 <div key={prod.id} className="producto">
                     <img src={prod.img} alt={prod.nombre} />
                     <h3>{prod.nombre}</h3>
+                    <p className="ensamblado">Sin Ensamblar</p>
                     <p>${prod.preciosinensamblar}</p>
+                    <p className="ensamblado">Ensamblado</p>
                     <p>${prod.precioensamblado}</p>
                      <button className="comprar">Comprar</button>
                 </div>
